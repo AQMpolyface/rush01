c94d85f30050bd0f75456137fb5a30c54f2c4788
Morisoli Jonas <mjonas@c2r3s5.42lausanne.ch>
2026-07-04 11:16:01 +0200

    sec

--- ex00/solve.c
+++ ex00/solve.c
@@ -1,5 +1,22 @@
 #include <stdlib.h>
 
-int	**ft_solve_matrix(int const *input, int **matrix)
+int	ft_solve_matrix(int *input, int **matrix)
 {
+	j
+}
+
+int	ft_verify_board(int *input, int **matrix)
+{
+	int	i;
+	int	j;
+	int	top_right;
+	int	bottom_left;
+
+	i = 0;
+	j = 0;
+	while (i < 12)
+	{
+		top_right = input[i];
+		bottom_left = input[i + 4];
+	}
 }
