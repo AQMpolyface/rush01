3e9bae9857c22da23acef41b2a3cae099d6ee682
polyface <polyface@proton.me>
2026-07-05 12:06:17 +0200

    AHGHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- ex00/main.c
+++ ex00/main.c
@@ -16,28 +16,30 @@ int	main(int argc, char **argv)
 	if (!input)
 		return (1);
 
-	ft_print_input(input);
 	ft_putchar('\n');
 	if (!matrix)
 		return (1);
 */
-	// int *input = ft_parse_input("3 1 2 2 2 3 2 1 2 2 1 4 2 2 4 1");
-	int *input = ft_parse_input("4 3 2 1 1 2 2 2 4 3 2 1 1 2 2 2");
+
+	// int *input = ft_parse_input("2 1 2 3 2 3 2 1 2 1 2 2 3 4 2 1");
+	int *input = ft_parse_input("3 1 2 2 2 3 2 1 2 2 1 4 2 2 4 1");
+	//  basecase	i
+	// int *input = ft_parse_input("4 3 2 1 1 2 2 2 4 3 2 1 1 2 2 2");
 	int **matrix = ft_alloc_matrix();
 
 	// int matrix_cpy[4][4] = {
 	// 	{1, 2, 3, 4},
 	// 	{2, 3, 4, 1},
 	// 	{3, 4, 1, 2},
-	// 	{4, 1, 0, 0},
+	// 	{4, 1, 2, 3},
 	// };
 
 	// for (int i = 0; i < 4; i++)
 	// 	for (int j = 0; j < 4; j++)
 	// 		matrix[i][j] = matrix_cpy[i][j];
+	// printf("%d \n", ft_board_contains_duplicate(matrix));
 	// ft_print_matrix(matrix);
-	printf("%d\n", ft_is_valid_board(input, matrix));
-	printf("%d\n", solve(input, matrix));
+	solve(input, matrix, 0, 0);
 	// ft_put_y_right(input, matrix);
 	// ft_put_y_left(input, matrix);
 	// ft_put_x_bottom(input, matrix);
