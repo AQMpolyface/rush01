fc15787e1c8ba9bb71ffca4d0584d38a6bde20c7
polyface <polyface@proton.me>
2026-07-05 09:49:49 +0200

    mewomomrqnojgrwjonigrno

---
+++ ex00/put.h
@@ -0,0 +1,5 @@
+
+int	max_put_x_top(int *input, int **matrix);
+
+int	max_put_y_left(int *input, int **matrix);
+
--- ex00/solve.h
+++ ex00/solve.h
@@ -1,5 +1,9 @@
-int	**ft_solve_matrix(int *input, int **matrix);
+int	**ft_is_valid_board(int *input, int **matrix);
 
 int ft_verify_row(int row, int* input, int** matrix);
 
 int ft_verify_column(int col, int* input, int** matrix);
+
+int	max_pose(int *input, int **matrix);
+
+int	solve(int *input, int **matrix, int x, int num);
