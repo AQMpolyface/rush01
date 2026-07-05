6f931b3e01076c187184640df7a9a4e8b696e3a0
polyface <polyface@proton.me>
2026-07-05 14:50:47 +0200

    sdfm

--- ex00/main.c
+++ ex00/main.c
@@ -7,26 +7,24 @@
 int	main(int argc, char **argv)
 
 {
-	// if (argc <= 1)
-	// {
-	// 	ft_putstr("usage: ./rush01 col1top col2top col3top col4top col1bottom col2bottom col3bottom col4bottom row1left row2left row3left row4left row1right row2right row3right row4right");
-	// 	return (0);
-	// }
-	/*int *input = ft_parse_input(argv[1]);
+	if (argc <= 1)
+	{
+		ft_putstr("usage: ./rush01 col1top col2top col3top col4top col1bottom col2bottom col3bottom col4bottom row1left row2left row3left row4left row1right row2right row3right row4right");
+		return (0);
+	}
+	int *input = ft_parse_input(argv[1]);
 	if (!input)
 		return (1);
 
-	ft_putchar('\n');
-	if (!matrix)
-		return (1);
-*/
 
 	// int *input = ft_parse_input("2 1 2 3 2 3 2 1 2 1 2 2 3 4 2 1");
-	int *input = ft_parse_input("3 1 2 2 2 3 2 1 2 2 1 4 2 2 4 1");
+	// int *input = ft_parse_input("3 1 2 2 2 3 2 1 2 2 1 4 2 2 4 1");
 	//  basecase	i
 	// int *input = ft_parse_input("4 3 2 1 1 2 2 2 4 3 2 1 1 2 2 2");
 	int **matrix = ft_alloc_matrix();
 
+	if (!matrix)
+		return (1);
 	// int matrix_cpy[4][4] = {
 	// 	{1, 2, 3, 4},
 	// 	{2, 3, 4, 1},
@@ -46,7 +44,6 @@ int	main(int argc, char **argv)
 	// ft_put_x_top(input, matrix);
 	ft_print_matrix(matrix);
 	ft_putchar('\n');
-	/*
 		int i = 0;
 		while (i < 4)
 		{
@@ -55,6 +52,5 @@ int	main(int argc, char **argv)
 		}
 		free(matrix);
 		free(input);
-	*/
-	return (0);
+		return (0);
 }
